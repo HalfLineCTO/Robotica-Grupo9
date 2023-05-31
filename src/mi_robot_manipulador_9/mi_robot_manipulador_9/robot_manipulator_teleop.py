2403f6e6012f68757034d25b5f934f8372845c53
Juan Esteban <je.cardona@uniandes.edu.co>
2023-05-31 14:37:06 -0500

    git commit -m "Ahora se manipua con numeros" git add git push origin main

--- src/mi_robot_manipulador_9/mi_robot_manipulador_9/robot_manipulator_teleop.py
+++ src/mi_robot_manipulador_9/mi_robot_manipulador_9/robot_manipulator_teleop.py
@@ -1,6 +1,6 @@
 import rclpy                   # Importa el módulo de Python 'rclpy', que proporciona la API de ROS2 para Python
 from rclpy.node import Node    # Importa la clase 'Node' de 'rclpy.node'
-from geometry_msgs.msg import Vector3   # Importa el mensaje 'Vector3' del paquete 'geometry_msgs'
+from geometry_msgs.msg import Twist   # Importa el mensaje 'Vector3' del paquete 'geometry_msgs'
 import sys, select, tty, termios    # Importa algunos módulos de Python
 from pynput import keyboard as kb
 
@@ -10,76 +10,96 @@ class TeleopManipulador(Node):    # Define una clase tipo 'Node'
         def __init__(self):    # Define un método llamado '__init__' que se ejecutará cuando se cree una instancia de la clase
                 super().__init__('robot_manipulador_teleop')    # Llama al método '__init__' de la clase 'Node' y le da un nombre al nodo
                 
-                self.publisher_ = self.create_publisher(Vector3, 'robot_manipulador_position', 10) #Publisher del mensaje Vector3
+                self.publisher_ = self.create_publisher(Twist, 'robot_manipulador_teleop', 10) #Publisher del mensaje Vector3
                 
                 
-                print("Ingrese los parametros")
+                
                 # self.paso_lineal  = float(input("Ingrese tamaño del paso en grados entre 0 y 180: "))
-                self.paso_angular  = float(input("Ingrese tamaño del paso en grados entre 0 y 180: ")) #Prompt para ingresar el tamaño del paso para los servos
-    
+                self.paso_angular  = float(70) #Prompt para ingresar el tamaño del paso para los servos
+                print("Listo para teleoperar el robot manipulador")
+
         def on_press(self, key): 
         
-                self.Vector3 = Vector3() 
+                self.manip._1 = Twist() 
+                self.manip._2 = Twist() 
+                self.manip._3 = Twist() 
+                self.manip._4 = Twist() 
         
-                if key == kb.KeyCode.from_char('q'):    # Si la tecla pulsada es 'q', establece paso angular positivo para el servo 1
-                        self.Vector3._x= self.paso_angular #El movimiento del servo 1 se guarda en la componente x del mensaje Vector3
-                        resp = print('Estas pulsando la tecla q')
-                elif key == kb.KeyCode.from_char('Q'):    # Si la tecla es 'Q', Invierte el sentido de giro del servo 1
-                        self.Vector3._x= -self.paso_angular
-                        resp =  print('Estas pulsando la tecla Q')
-                elif key == kb.KeyCode.from_char('e'):    # Con la tecla 'e', se establece paso angular positivo para el servo 2
-                        self.Vector3._y= self.paso_angular #El movimiento del servo 2 se guarda en la componente y del mensaje Vector3
-                        resp =  print('Estas pulsando la tecla e')
-                elif key == kb.KeyCode.from_char('E'):    # Si la tecla es 'E', Invierte el sentido de giro del servo 2
-                        self.Vector3._y= -self.paso_angular
-                        resp = print('Estas pulsando la tecla E')
-                elif key == kb.KeyCode.from_char('f'):    # Con la tecla 'f', se establece paso angular positivo para el servo 3
-                        self.Vector3._z= self.paso_angular #El movimiento del servo 3 se guarda en la componente z del mensaje Vector3
-                        resp =  print('Estas pulsando la tecla f')
-                elif key == kb.KeyCode.from_char('F'):    # si  la tecla es 'F', Invierte el sentido de giro del servo 3
-                        self.Vector3._z= -self.paso_angular
-                        resp =  print('Estas pulsando la tecla F')
+                if key == kb.KeyCode.from_char('1'):    # Si la tecla pulsada es '1', establece paso angular positivo para el servo 1
+                        self.manip._1= self.paso_angular #El movimiento del servo 1 
+                        resp = print('Estas pulsando la tecla 1')
+                elif key == kb.KeyCode.from_char('2'):    # Si la tecla es '2', Invierte el sentido de giro del servo 2
+                        self.manip._1= -self.paso_angular
+                        resp =  print('Estas pulsando la tecla 2')
+                elif key == kb.KeyCode.from_char('3'):    # Con la tecla '3', se establece paso angular positivo para el servo 2
+                        self.manip._2= self.paso_angular #El movimiento del servo 2 
+                        resp =  print('Estas pulsando la tecla 3')
+                elif key == kb.KeyCode.from_char('4'):    # Si la tecla es '4', Invierte el sentido de giro del servo 2
+                        self.manip._2= -self.paso_angular
+                        resp = print('Estas pulsando la tecla 4')
+                elif key == kb.KeyCode.from_char('5'):    # Con la tecla '5', se establece paso angular positivo para el servo 3
+                        self.manip._3= self.paso_angular #El movimiento del servo 3 
+                        resp =  print('Estas pulsando la tecla 5')
+                elif key == kb.KeyCode.from_char('6'):    # si  la tecla es '6', Invierte el sentido de giro del servo 3
+                        self.manip._3= -self.paso_angular
+                        resp =  print('Estas pulsando la tecla 6')
+                elif key == kb.KeyCode.from_char('7'):    # Con la tecla '7', se establece paso angular positivo para el servo 4
+                        self.manip._3= self.paso_angular #El movimiento del servo 4
+                        resp =  print('Estas pulsando la tecla 7')
+                elif key == kb.KeyCode.from_char('8'):    # si  la tecla es '8', Invierte el sentido de giro del servo 4
+                        self.manip._3= -self.paso_angular
+                        resp =  print('Estas pulsando la tecla 8')
                 else:    # Si no se presionó ninguna de las teclas anteriores, establece ambas velocidades en cero
-                        self.Vector3._x= self.paso_angular= 0.0 
-                        self.Vector3._y= self.paso_angular= 0.0 
-                        self.Vector3._z= self.paso_angular= 0.0 
-                self.get_logger().info('Sending - Paso angular  : %f' % (self.Vector3._x,self.Vector3._x,self.Vector3._x))
-                self.publisher_.publish(self.Vector3)  
+                        self.manip._1= self.paso_angular= 0.0 
+                        self.manip._2= self.paso_angular= 0.0 
+                        self.manip._3= self.paso_angular= 0.0 
+                        self.manip._2= self.paso_angular= 0.0 
+                self.get_logger().info('Sending - Paso angular  : %f' % (self.manip._1,self.manip._2,self.manip._3,self.manip._4))
+                self.publisher_.publish(self.manip)  
 
         def on_release(self, key): #Al soltar la tecla vuelve todos los valores a cero
         
-                self.Vector3 = Vector3() 
+                self.manip._1 = Twist() 
+                self.manip._2 = Twist() 
+                self.manip._3 = Twist() 
+                self.manip._4 = Twist() 
         
-                if key == kb.KeyCode.from_char('q'):    # 
-                        self.Vector3._x= 0.0
-                        resp = print('Estas pulsando la tecla q')
-                elif key == kb.KeyCode.from_char('Q'):    # 
-                        self.Vector3._x= -0.0
-                        resp =  print('Estas pulsando la tecla Q')
-                elif key == kb.KeyCode.from_char('e'):    # 
-                        self.Vector3._y= 0.0
-                        resp =  print('Estas pulsando la tecla e')
-                elif key == kb.KeyCode.from_char('E'):    # 
-                        self.Vector3._y= -0.0
-                        resp = print('Estas pulsando la tecla E')
-                elif key == kb.KeyCode.from_char('f'):    #
-                        self.Vector3._z= 0.0
-                        resp =  print('Estas pulsando la tecla f')
-                elif key == kb.KeyCode.from_char('F'):    # 
-                        self.Vector3._z= -0.0
-                        resp =  print('Estas pulsando la tecla F')
+                if key == kb.KeyCode.from_char('1'):    # 
+                        self.manip._1= 0.0
+                        resp = print(' Dejó de pulsar la tecla 1')
+                elif key == kb.KeyCode.from_char('2'):    # 
+                        self.manip._1= -0.0
+                        resp =  print('Dejó de pulsar  la tecla 2')
+                elif key == kb.KeyCode.from_char('3'):    # 
+                        self.manip._2= 0.0
+                        resp =  print('Dejó de pulsar  la tecla 3')
+                elif key == kb.KeyCode.from_char('4'):    # 
+                        self.manip._2= -0.0
+                        resp = print(' Dejó de pulsar la tecla 4')
+                elif key == kb.KeyCode.from_char('5'):    #
+                        self.manip._3= 0.0
+                        resp =  print('Dejó de pulsar  la tecla 5')
+                elif key == kb.KeyCode.from_char('6'):    # 
+                        self.manip._3= -0.0
+                        resp =  print('Dejó de pulsar  la tecla 6')
+                elif key == kb.KeyCode.from_char('7'):    #
+                        self.manip._4= 0.0
+                        resp =  print('Dejó de pulsar  la tecla 7')
+                elif key == kb.KeyCode.from_char('8'):    # 
+                        self.manip._4= -0.0
+                        resp =  print('Dejó de pulsar  la tecla 8')
                 else:    # Si no se presionó ninguna de las teclas anteriores, establece ambas velocidades en cero
-                        self.Vector3._x= self.paso_angular= 0.0 
-                        self.Vector3._y= self.paso_angular= 0.0 
-                        self.Vector3._z= self.paso_angular= 0.0 
-                self.get_logger().info('Sending - Paso angular  : %f' % (self.Vector3._x,self.Vector3._x,self.Vector3._x))
-                self.publisher_.publish(self.Vector3)  
+                        self.manip._x= self.paso_angular= 0.0 
+                        self.manip._y= self.paso_angular= 0.0 
+                        self.manip._z= self.paso_angular= 0.0 
+                self.get_logger().info('Sending - Paso angular  : %f' % (self.manip._1,self.manip._2,self.manip._3,self.manip._4))
+                self.publisher_.publish(self.manip)  
 def main():
     rclpy.init()    # Inicializa ROS2
     robot_manipulador_teleop = TeleopManipulador()    # Crea una instancia de la clase 
     with kb.Listener(on_press= robot_manipulador_teleop.on_press,on_release = robot_manipulador_teleop.on_release) as escuchador:
         escuchador.join()
-    print('Enviando datos a la raspberry')
+    print('Enviando datos al robot')
     rclpy.spin(robot_manipulador_teleop)    # Inicia el bucle de eventos de ROS2
     robot_manipulador_teleop.destroy_node()    # Destruye el nodo cuando se cierra el bucle de eventos
     rclpy.shutdown()    # Cierra ROS2
